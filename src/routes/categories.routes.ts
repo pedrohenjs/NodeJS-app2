d5950193554ec6b18dcb458f87efaf64defa11f2
Pedro <pedrojs.contato@gmail.com>
2022-06-20 19:38:52 -0300

    new-commit

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { CreateCategoryService } from '../services/createCategoryService'
 import CategoriesRepository from '../repositories/categoriesRepository'
 
 const categoriesRoutes = Router()
@@ -6,6 +7,9 @@ const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
+
+  createCategoryService.execute({ name, description })
 
   return response.status(201).send()
 })
